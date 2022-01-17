0c22eb54512420685031d390fa8fc992c7f4bb45
zeroradish <mr.zeroradish@gmail.com>
2022-01-18 01:32:32 +0900

    chore: baekjoon 1000

---
+++ src/zeroradish/baekjoon/_1000.java
@@ -0,0 +1,12 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.print(num1 + num2);
+    }
+}
