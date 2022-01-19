082e01082f3250433afeb3453ccf4a49b353ff29
yongbeom <joyowlq@naver.com>
2022-01-19 17:14:25 +0900

    chore: baekjoon 10171

---
+++ src/joyowlsf/baekjoon/_10171.java
@@ -0,0 +1,11 @@
+package joyowlsf.baekjoon;
+
+public class _10171 {
+        public static void main(String[] args){
+            System.out.println("\\    /\\");
+            System.out.println(" )  ( ')");
+            System.out.println("(  /  )");
+            System.out.println(" \\(__)|");
+        }
+}
+
