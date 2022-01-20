b267ff765c99555ef148f8334b381d9c26220b44
spidyweb <jiho358896@gmail.com>
2022-01-20 09:06:04 +0900

    chore: baekjoon 10718

---
+++ src/spidyweb/baekjoon/_10718.py
@@ -0,0 +1,18 @@
+#방법 1
+print("강한친구 대한육군\n"+"강한친구 대한육군")
+"""
+#방법 2
+print("강한친구 대한육군\n"+"강한친구 대한육군")
+#방법 3
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+#방법 4
+content="강한친구 대한육군"
+print(content+"\n"+content)
+#방법 5
+content="강한친구 대한육군"
+print(f"{content}\n{content}")
+#방법 6
+content="강한친구 대한육군"
+print("%s\n%s" % (content,content))
+"""
