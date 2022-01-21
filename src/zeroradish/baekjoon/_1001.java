a79d8adda1b353da0eba9c8966b3426651806e5e
zeroradish <mr.zeroradish@gmail.com>
2022-01-21 16:21:57 +0900

    chore: baekjoon 1001

---
+++ src/zeroradish/baekjoon/_1001.java
@@ -0,0 +1,12 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.print(num1-num2);
+    }
+}
