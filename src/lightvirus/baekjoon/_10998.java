a9ac5db952edecf5507a48e2080573b204d66174
kyun-9458 <jkk9458@gmail.com>
2022-01-23 11:08:05 +0900

    chore: baekjoon 10998

---
+++ src/lightvirus/baekjoon/_10998.java
@@ -0,0 +1,14 @@
+package lightvirus.baekjoon;
+
+import java.util.Scanner;
+
+public class _10998 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a*b);
+    }
+}
