a35e48e7f6e17d8385c1ababb9d65a058afcf628
yongbeom <joyowlq@naver.com>
2022-01-28 13:58:03 +0900

    chore: baekjoon 1000

---
+++ src/joyowlsf/baekjoon/_1000.java
@@ -0,0 +1,13 @@
+package joyowlsf.baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class _1000 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
