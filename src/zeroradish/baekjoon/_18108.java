5c3e9bbfa792c9fbc2ced538c395b6b29b059440
zeroradish <mr.zeroradish@gmail.com>
2022-01-29 23:34:42 +0900

    chore: baekjoon 18108

---
+++ src/zeroradish/baekjoon/_18108.java
@@ -0,0 +1,10 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _18108 {
+    public static void main(String[] args) {
+        //스리랑카 및 태국에 전해 온다. 현재 태국에서는 서기 연도에 543을 더한 연도를 태국 자체의 불기인 태국 태양력으로 사용하고 있다.
+        System.out.println(new Scanner(System.in).nextInt() -543);
+    }
+}
