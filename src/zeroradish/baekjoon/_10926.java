63e85c1aa83cbe318264e0d01d75f6d1d1205f04
zeroradish <mr.zeroradish@gmail.com>
2022-01-29 23:09:00 +0900

    chore: baekjoon 10926

---
+++ src/zeroradish/baekjoon/_10926.java
@@ -0,0 +1,9 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _10926 {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).next() + "??!");
+    }
+}
