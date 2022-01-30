d3d9a723340988e22b27bb12269d48aeced4bff4
zeroradish <mr.zeroradish@gmail.com>
2022-01-30 16:03:42 +0900

    chore: baekjoon 1330

---
+++ src/zeroradish/baekjoon/_1330.java
@@ -0,0 +1,18 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _1330 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        if (num1 > num2) {
+            System.out.println(">");
+        } else if (num1 < num2) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
