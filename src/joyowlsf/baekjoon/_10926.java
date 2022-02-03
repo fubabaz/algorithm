8afc3ea8b2d565a41d23139c5dac72028f991132
yongbeom <joyowlq@naver.com>
2022-02-03 10:35:52 +0900

    chore: baekjoon 10926

---
+++ src/joyowlsf/baekjoon/_10926.java
@@ -0,0 +1,11 @@
+package joyowlsf.baekjoon;
+
+import java.util.Scanner;
+
+public class _10926 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        System.out.println(id+"??!");
+    }
+}
