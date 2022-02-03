d84cdebcda12c68d71c3ec2f00605997882727eb
yongbeom <joyowlq@naver.com>
2022-02-03 10:29:48 +0900

    chore: baekjoon 10869

---
+++ src/joyowlsf/baekjoon/_10869.java
@@ -0,0 +1,16 @@
+package joyowlsf.baekjoon;
+
+import java.util.Scanner;
+
+public class _10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
