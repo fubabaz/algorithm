d4fc46894b13dfd51d52ded7b0d5a45987b2fcd5
spidyweb <jiho358896@gmail.com>
2022-02-03 10:11:52 +0900

    chore: baekjoon 10869

---
+++ src/spidyweb/baekjoon/_10869.py
@@ -0,0 +1,11 @@
+#방법 1
+A,B=map(int, input().split())
+print(f"{A+B}\n{A-B}\n{A*B}\n{int(A/B)}\n{A%B}")
+
+#방법 2
+"""A,B=map(int, input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)"""
