455ee050ca0cfdf936e188a657acf3151f6639d2
yongbeom <joyowlq@naver.com>
2022-02-07 12:45:45 +0900

    chore: baekjoon 1330

---
+++ src/joyowlsf/baekjoon/_1330.java
@@ -0,0 +1,20 @@
+package joyowlsf.baekjoon;
+
+import java.util.Scanner;
+
+public class _1330 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
