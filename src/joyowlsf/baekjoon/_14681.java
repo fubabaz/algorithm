96ae94ece166368c57911e31f1e9c8f00022b02c
yongbeom <joyowlq@naver.com>
2022-02-08 11:33:22 +0900

    chore: baekjoon 14681

---
+++ src/joyowlsf/baekjoon/_14681.java
@@ -0,0 +1,21 @@
+package joyowlsf.baekjoon;
+
+import java.util.Scanner;
+
+public class _14681 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a>0 && b>0){
+            System.out.println(1);
+        } else if (a<0 && b>0){
+            System.out.println(2);
+        } else if (a<0 && b<0){
+            System.out.println(3);
+        } else
+            System.out.println(4);
+    }
+}
