be489d961e26caad78887ba1aab17a6782205248
zeroradish <mr.zeroradish@gmail.com>
2022-02-09 00:34:02 +0900

    chore: baekjoon 14681

---
+++ src/zeroradish/baekjoon/_14681.java
@@ -0,0 +1,15 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) System.out.println("1");
+        else if (x < 0 && y > 0) System.out.println("2");
+        else if (x < 0 && y < 0) System.out.println("3");
+        else System.out.println("4");
+    }
+}
