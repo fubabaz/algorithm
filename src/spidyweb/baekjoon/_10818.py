64293b3b8f86599ddbf64cf112aff53b5d62c33c
spidyweb <jiho358896@gmail.com>
2022-03-12 09:56:10 +0900

    chore: baekjoon 10818

---
+++ src/spidyweb/baekjoon/_10818.py
@@ -0,0 +1,13 @@
+#python에서는 C,C++등과 다르게 배열에 입력 받을 때 Test Case의 갯수와 상관이 없다.
+N = int(input())
+arr = list(map(int, input().split()))
+max = arr[0]
+min = arr[0]
+
+for i in arr:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+
+print(min, max)
