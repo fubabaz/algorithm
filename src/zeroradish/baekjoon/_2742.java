7d63af9195ff07b7e5dd671a5d5a03ab7a78d319
mr.zeroradish@gmail.com <mr.zeroradish@gmail.com>
2022-03-17 23:09:42 +0900

    chore: baekjoon 2742

---
+++ src/zeroradish/baekjoon/_2742.java
@@ -0,0 +1,13 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _2742 {
+    public static void main(String[] args)   {
+        int count =  new Scanner(System.in).nextInt();
+        while(count != 0 ) {
+            System.out.println(count);
+            count--;
+        }
+    }
+}
