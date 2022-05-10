b83ff1e2d0d2e6ae921e756f8c8bc61cb7ea771f
yongbeom <joyowlq@naver.com>
2022-05-10 14:28:49 +0900

    baekjoon: 1316

---
+++ src/joyowlsf/baekjoon/_1316.py
@@ -0,0 +1,13 @@
+a = int(input())
+cnt = a
+
+for i in range(a):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
