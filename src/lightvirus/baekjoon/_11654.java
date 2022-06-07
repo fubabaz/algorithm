a746d28ddd3e1d07ac9f41418ab2f046237e84cb
kyun-9458 <jkk9458@gmail.com>
2022-06-07 09:41:24 +0900

    baekjoon: 11654

---
+++ src/lightvirus/baekjoon/_11654.java
@@ -0,0 +1,15 @@
+package lightvirus.baekjoon;
+
+import java.util.Scanner;
+
+public class _11654 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char s = sc.nextLine().charAt(0);
+
+        int num = (int)s;
+
+        System.out.println(num);
+    }
+}
