c9c8b3fa80b4d30869c1220528e2074379fb9b69
mr.zeroradish@gmail.com <mr.zeroradish@gmail.com>
2022-08-31 08:16:32 +0900

    baekjoon 11654

---
+++ src/zeroradish/baekjoon/_11654.java
@@ -0,0 +1,9 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _11654 {
+    public static void main(String[] args) {
+        System.out.print( (byte) new Scanner(System.in).next().charAt(0));
+    }
+}
