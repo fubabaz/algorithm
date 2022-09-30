50a3ca30a9a57ad9c51a39dd357da929586d5b6c
spidyweb <jiho358896@gmail.com>
2022-09-30 10:59:35 +0900

    [PYTHON] baekjoon 2775 부녀회장이될테야

--- src/spidyweb/baekjoon/python/_2775.py
+++ src/spidyweb/baekjoon/python/_2775.py
@@ -1,13 +1,13 @@
-def get_people_num(T):
+def get_people_num(T=int(input())):
     for _ in range(T):
         k = int(input())
         n = int(input())
-        people_num = [ i for i in range(1, n+1)]
+        people_num = [i for i in range(1,n+1)] # k=1 n =3 일때, 1,2,3이 배열에 담김
         for __ in range(k):
             for j in range(1,n):
-                people_num[j] += people_num[j-1]
+                people_num[j] += people_num[j-1] # n=3이므로 배열의 각 자리는
         print(people_num[-1])
-    return
+    return None
 
 if __name__=="__main__":
-    get_people_num(int(input()))
+    get_people_num()
