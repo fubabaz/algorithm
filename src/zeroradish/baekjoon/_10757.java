02e5696854e06d416136c5adf7f749eba0d20e04
zeroradish <mr.zeroradish@gmail.com>
2022-10-06 08:22:42 +0900

    [JAVA]baekjoon 10757 큰 수 A+B

---
+++ src/zeroradish/baekjoon/_10757.java
@@ -0,0 +1,13 @@
+package zeroradish.baekjoon;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class _10757 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigDecimal n1 = sc.nextBigDecimal();
+        BigDecimal n2 = sc.nextBigDecimal();
+        System.out.print(n1.add(n2));
+    }
+}
