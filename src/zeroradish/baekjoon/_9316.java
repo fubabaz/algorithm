8c26b1dad3b56e9327312abfc63cf0ffbd912c81
zeroradish <mr.zeroradish@gmail.com>
2022-11-30 09:36:15 +0900

    Bronze4: baekjoon 9316 Hello Judge

---
+++ src/zeroradish/baekjoon/_9316.java
@@ -0,0 +1,12 @@
+package zeroradish.baekjoon;
+
+import java.util.Scanner;
+
+public class _9316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++)
+            System.out.printf("Hello World, Judge %d!%n", i);
+    }
+}
