41b2d642d10f7e34aa069265029481a200747b44
spidyweb <jiho358896@gmail.com>
2023-01-14 10:37:38 +0900

    Bronze2: baekjoon 2750 수 정렬하기

--- src/spidyweb/baekjoon/python/_2750.py
+++ src/spidyweb/baekjoon/python/_2750.py
@@ -7,7 +7,7 @@ for x in range(N):
     num_list.append(i)
 
 for i in range(len(num_list)):
-    for j in range(len(num_list)):
+    for j in range(len(num_list)): # 버블정렬: 앞의 인덱스의 수와 비교하여 현재 인덱스의 수가 더 크면 숫자를 바꿈
         if num_list[i] < num_list[j]:
             num_list[i], num_list[j] = num_list[j], num_list[i]
 
