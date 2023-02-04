536f5b17ba349980769efd41fd9a9f2ebf112a08
arhan0109 <hanaro0109@gmail.com>
2023-02-04 22:41:46 +0900

    Silver5: baekjoon 2751 수 정렬하기 2

--- src/arhan0109/beakjoon/_2751.java
+++ src/arhan0109/beakjoon/_2751.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class _2751 {
+package beakjoon;
+public class _2751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        int count = sc.nextInt();
+        ArrayList<Integer> num = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            num.add(sc.nextInt());
+        }
+        Collections.sort(num);
+        for(int i : num){
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
