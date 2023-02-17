1c4f3eb9247859695aaaca70d5ea7ad545c6c2d0
arhan0109 <hanaro0109@gmail.com>
2023-02-17 23:33:52 +0900

    Silver5: baekjoon 2751 수 정렬하기 2

--- src/arhan0109/beakjoon/_10989.java
+++ src/arhan0109/beakjoon/_10989.java
@@ -1,2 +1,32 @@
-package beakjoon;public class _10989 {
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10989 {
+    public static void main(String[] args) throws IOException {
+
+        int[] cnt = new int[10001];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            cnt[Integer.parseInt(br.readLine())] ++;
+        }
+
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < 10001; i++){
+            while(cnt[i] > 0){
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
+        }
+        System.out.println(sb);
+    }
 }
