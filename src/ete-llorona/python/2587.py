c6a122691b6390c43f6356eaeb83685121037d4a
zeroradish <mr.zeroradish@gmail.com>
2024-05-09 09:12:06 +0900

    chore: 활동을 기대 할 수 없는 인원의 패키지 삭제

--- src/ete-llorona/python/2587.py
+++
@@ -1,9 +0,0 @@
-lst = [int(input()) for _ in range(5)]
-avg = sum(lst)//5
-for k in range(4):
-    for i in range(4):
-        if lst[i] > lst[i+1]:
-            lst[i], lst[i+1] = lst[i+1], lst[i]
-median = lst[2]
-print(avg)
-print(median)
